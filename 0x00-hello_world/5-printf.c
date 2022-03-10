a3360f1124421b141f6802dd5a9f6ebdb5c08dac
amlaku <amlakubeyene@gmail.com>
2022-03-10 14:19:07 +0300

    Write a C program that prints exactly with proper grammar

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - print quote by using printf
+ * Description: but the outcome is a piece of art
+ * Return: 0
+ */
+int main(void)
+{
+
+	printf("%s\n", "with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
