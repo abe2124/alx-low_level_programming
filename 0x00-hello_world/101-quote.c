9788686075eeba7c084d8f93d31301f28aa9a5e0
amlaku <amlakubeyene@gmail.com>
2022-03-10 16:27:13 +0300

    OMG

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,6 +4,7 @@
  * main - print error
  * Description: not allowed to use any functions
  * Return: return 1
+ */
 int main(void)
 {
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
