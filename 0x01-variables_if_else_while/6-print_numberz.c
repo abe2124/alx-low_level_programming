785dd619d9cee710af39a63499ecfee4dc46a498
amlaku <amlakubeyene@gmail.com>
2022-03-11 23:22:08 +0300

    print single digit number base 10

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 /**
  * main - main block
- * Description:prints all single digit numbers
- * base 10 
+ * Description:prints all single digit numbers of base 10
+ * starting from 0 fllowd by a new line. 
  * Return: 0
  */
 int main(void)
