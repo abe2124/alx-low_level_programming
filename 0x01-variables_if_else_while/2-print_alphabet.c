1e09c0bf63a570f21967bf6f5d136de7b23604a8
amlaku <amlakubeyene@gmail.com>
2022-03-11 22:31:39 +0300

    alphabat in lower

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - alphabet
+ * Description: prints the alphabet in lower
+ * Return: 0
+ */
+int main(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		putchar(c);
+		c++;
+	}
+	putchar('\n');
+	return (0);
+}
