90c49c5254927083f683a15c60f32676fc1ec50b
amlaku <amlakubeyene@gmail.com>
2022-03-11 22:40:23 +0300

    print alphabet on both uper and lower floowd by new line

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main -main alphabat in both
+ * Description: prints the alphabet in both lower and uper
+ * fllowd by a new line
+ * Return: 0
+ */
+int main(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		putchar(c);
+		c++;
+	}
+	c = 'A';
+	
+	while (c <= 'Z')
+	{
+		putchar(c);
+		c++;
+	}
+	
+	putchar('\n');
+	return (0);
+}
