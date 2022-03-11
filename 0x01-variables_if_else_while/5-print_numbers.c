55f9f232d0827fd98dd54a40071c78754dfbd607
amlaku <amlakubeyene@gmail.com>
2022-03-11 23:08:31 +0300

    digits from 0 to 9

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+/**
+ * main - digit from zero
+ * Description: prints all single digit numbers of base 10
+ * starting from 0 fllowd by a new line
+ * Return: 0
+ */
+int main(void)
+{
+	char a = '0';
+
+	while (c <= '9')
+	{
+		putchar(c);
+		c++;
+	}
+	putchar('\n');
+	return (0);
+}
