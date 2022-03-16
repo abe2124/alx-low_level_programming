f31a31b91759fe8d7937fbc19202d8a5a5ed2ec1
amlaku <amlakubeyene@gmail.com>
2022-03-16 17:40:55 +0300

    If we list all the natural numbers below 10 that are multiples of 3 or 5

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,47 @@
+#include "main.h"
+/**
+ * print_times_table - print n times
+ * @n: integer n
+ * Return void
+ */
+void print_times_table(int n)
+{
+	int r, c, p;
+
+	if (n >= 0 && n < 15)
+	{
+		for (r = 0; r < = n; r++)
+		{
+			for (c = 0; c <=n; c++)
+			{
+				p = r * c;
+				if (c == 0)
+					_putchar('0');
+				else if (p < 10')
+				{
+					_putchar(','); 
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(p  % 10 + '0');
+				}
+				else if (p >= 10 && p < 100)
+				{
+					_putchar(','); 
+					_putchar(' ');
+					_putchar(' ');
+					_putchar((p / 10) % 10 + '0');
+					_putchar(p % 10 + '0');
+				}
+				else if (p > 99 && p < 1000)
+				{
+					_putchar(','); 
+					_putchar(' ');
+					_putchar(p / 10 + '0');
+					_putchar((p /10) % 10 + '0');
+					_putchar(p % 10 + '0');
+				}
+			}
+			_putchar('\n');
+		}
+	}
+}
