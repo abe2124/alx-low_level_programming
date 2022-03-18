55b1e69bd4ec5e52d7c47a4d78c8c2a5ffb08bba
amlaku <amlakubeyene@gmail.com>
2022-03-18 12:54:46 +0300

    Fizz-Buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -8,7 +8,7 @@ int main(void)
 {
 	int i;
 
-	for (i = 0; i <= 100; i++)
+	for (i = 1; i <= 0; i++)
 	{
 		if (i % 3 == 0 || i % 5 == 0)
 		{
@@ -18,14 +18,18 @@ int main(void)
 			}
 			if (i % 5 == 0)
 			{
-				printf("Buzz");
+				printf("buzz");
 			}
-			else
-			{
-				printf(" ");
-			}
-			printf("\n");
-			return (0);
+		}
+		else
+		{
+			printf("%d", i);
+		}
+		if (i != 100)
+		{
+			printf(" ");
 		}
 	}
+	printf("\n");
+	return (0);
 }
