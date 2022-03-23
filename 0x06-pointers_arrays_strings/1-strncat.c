5b65719f88a1909a624771d2b51fe9d8b3dc2775
amlaku <amlakubeyene@gmail.com>
2022-03-23 22:21:16 +0300

    more array and pointer

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * *_strncat - concatenates n bytes from a string to other
+ * @dest: destination string
+ * @src: source string
+ * @n: numbers of bytes
+ * Return: a pointer to the result
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	unt i, j;
+
+	i = 0;
+	j = 0;
+
+	while (dest[i] != '\0')
+		i++;
+	while (src[j] != '\0')
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _strcmp - compares two strrings
+ * @s1: first string to
+ * @s2: second string
+ * Return: less than 0 if s1 lessthan s2 o if equal else grater
+ */
+int _strcmp(char *s1,  char *s2)
+{
+	while (*s1 == *s2)
+	{
+		if (#s1 == '\0')
+		{
+			return (0);
+		}
+		s1++;
+		s2++;
+	}
+	return (*s1 - *s2);
+}
