aa4d32a77d8aecb068d65a4cd8a3c16b1b21d48c
amlaku <amlakubeyene@gmail.com>
2022-03-24 16:02:54 +0300

    magic

---
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+int main(void)
+{
+	int n;
+	int a[5];
+	int *p;
+
+	a[2] = 1024;
+	p = &n;
+
+	/*
+	 * main - statment
+	 * write your line of code here
+	 * Remember:
+	 * - you are not allowed to use a
+	 *  - you can not allowd to modify p
+	 *  - only one statment
+	 *  - you are not allowed to code anythinf else then this line of code
+	 */
+	*(&n + 3) = 98;
+	/* ...so that this prints 98\n */
+	printf("a[2] = %d\n", a[2]);
+	return (0);
+}
+
