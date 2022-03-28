ebb8aeb0491ee8a61709c5fd4225b6245c4da56a
amlaku <amlakubeyene@gmail.com>
2022-03-29 00:42:52 +0300

    alt

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,31 @@
+/**
+ * _strspn - a function that gets the
+ * length of  a prefix substring
+ * @s: pointer to string input
+ * @accept: substring prefix to look for
+ * Return: the number
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	int i, j, k;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		j = 0;
+		k = 1;
+		while (accept[j] != '\0')
+		{
+			if (s[i] == accept[j])
+			{
+				k = 0;
+				break;
+			}
+			j++
+		}
+		if (k == 1)
+			break;
+		i++;
+	}
+	return (i);
+}
