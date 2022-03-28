bee2594ef7ca62fd7ff21110ebd4d06285d1a162
amlaku beyene <amlakubeyene@gmail.com>
2022-03-28 20:59:02 +0300

    pointer points to

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -4,7 +4,7 @@
  * _strchr - function that locates a char in string.
  *
  * @s: string to receive and return
- * @c: character to recieve
+ * @c: character to recieves
  * Return: char
  */
 char *_strchr(char *s, char c)
