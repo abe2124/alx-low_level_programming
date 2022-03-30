1143f321d8010f243d2f5b945baecffadc831849
amlaku <amlakubeyene@gmail.com>
2022-03-30 21:08:35 +0300

    Recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _puts_recursion - a function that print a string
+ *         trailing new line
+ *@s: input string
+ *
+ * Return: nonnegative number (Success)
+ */
+
+void _puts_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		putchar(*s);
+	_puts_recursion(s + 1);
+	}
+	else
+		putchar('\n');
+}
