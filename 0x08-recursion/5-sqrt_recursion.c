f5d301268b10775fb358ae37a253e4b12d507b91
amlaku <amlakubeyene@gmail.com>
2022-03-30 23:33:30 +0300

    squear root

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,37 @@
+#include"main.h"
+#include<stdio.h>
+
+/**
+ * _sqrt - a function that
+ *          returns the natural square
+ *          root of anumber
+ *@n: input number
+ *@i: input number
+ *Return: Always 0
+ */
+
+int _sqrt(int n, int i)
+{
+	if (i * i > n)
+		return (-1);
+	if (i * i == n)
+		return (i);
+	else
+		return (_sqrt(n, i + 1));
+}
+
+/**
+ * _sqrt_recursion - function
+ *
+ * @n: the number
+ * Return: Always 0.
+ */
+
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+		return (-1);
+	if (n == 0 || n == 1)
+		return (n);
+	return (_sqrt(n, 0));
+}
