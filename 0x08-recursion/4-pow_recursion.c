9a1ce4f8855d54b1b4e31d46b2921bd9a649b414
amlaku <amlakubeyene@gmail.com>
2022-03-30 22:30:06 +0300

    factorial and _pow_recursion

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,26 @@
+/**
+ * _pow_recursion - a function that return
+ *                  the value of x raised
+ *                  to the pwoer of y
+ * @x: input integer
+ * @y: input integer
+ *
+ * Return: the value of x raised to the power of y
+ */
+
+int _pow_recursion(int x, int y)
+{
+	int even;
+
+	if (y < 0)
+		return (-1);
+	else if (y == 0)     /*x is the power of 0 is 1*/
+		return (1);
+	else if (y % 2 == 0)   /*if y is an even number*/
+	{
+		even = _pow_recursion(x, y / 2);
+		return (even * even);
+	}
+	else
+		return (x * _pow_recursion(x, y - 1));
+}
