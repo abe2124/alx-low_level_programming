ca16e7b6166f8c9d2ce3d0a37edae1256532c819
unknown <amlakubeyene@gmail.com>
2022-04-05 17:42:02 +0300

    add two numbers

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,73 +1,64 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <ctype.h>
-#include <string.h>
+#include<stdio.h>
+#include<stdlib.h>
 
 /**
- * check_num - check - string there are digit
- * @str: array str
- *
- * Return; Always 0 (Success)
- */
+* ft_atoi - function
+*
+* @str: the chaine
+* Return: Always 0.
+*/
 
-int check_num(char *str)
-
+int		ft_atoi(const char *str)
 {
-/*Declaring variables*/
-unsigned int count;
+	int		n;
+	int		sign;
 
-count = 0;
-while (count < strlen(str)) /*count string*/
+	n = 0;
+	while (*str == ' ' || *str == '\t' || *str == '\v' || *str == '\r' ||
+			*str == '\n' || *str == '\f')
+		str++;
+	sign = *str == '-' ? -1 : 1;
+	if (sign == -1 || *str == '+')
+		str++;
+	while (*str)
+	{
+		if (*str >= '0' && *str <= '9')
+			n = n * 10 + (*str - '0');
+		else
+		{
+			printf("Error\n");
+			exit(EXIT_SUCCESS);
+			/*break*/
+		}
+		str++;
+	}
+	return (n * sign);
 
-{
-if (!isdigit(str[count])) /*check if str there are digit*/
-{
-return (0);
-}
-
-count++;
-}
-return (1);
 }
 
 /**
- * main - Print the name of the program
- * @argc: Count arguments
- * @argv: Arguments
- *
- * return: Always 0 (Success)
- */
+* main - function
+*
+* @argv: the chaine of chaine
+* @argc: the int
+* Return: Always 0.
+*/
 
-int main(int argc, char *argv[])
-
+int main(int argc, char **argv)
 {
+	int i, som = 0, a;
 
-/*Declaring variables*/
-int count;
-int str_to_int;
-int sum = 0;
-
-count = 1;
-while (count < argc) /*Goes through the whole array*/
-{
-if(check_num(argv[count]))
-
-{
-str_to_int = atoi(argv[count]); /*ATOI --> convert string to int*/
-sum += str_to_int;
-}
-
-/*Condition if one of the number contains symbols that are not digits*/
-else
-{
-printf("Error\n");
-return (1);
-}
-
-count++;
-}
-
-printf("%d\n", sum); /*print sum*/
-
-return (0);
+	if (argc == 1)
+	{
+		printf("0\n");
+		return (0);
+	}
+	for (i = 1; i < argc; i++)
+	{
+		a = ft_atoi(argv[i]);
+		if (a >= 0)
+			som = som + a;
+	}
+	printf("%d\n", som);
+	return (0);
 }
